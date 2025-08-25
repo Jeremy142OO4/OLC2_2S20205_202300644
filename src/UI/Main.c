ec1d0eecacf3b1aea5fbb4f0d209c35f3fc9e6cf
Jeremy142OO4 <3141193530501@ingenieria.usac.edu.gt>
2025-08-25 00:48:27 -0600

    prueba

--- src/UI/Main.c
+++ src/UI/Main.c
@@ -11,7 +11,7 @@ typedef struct {
     GtkListStore *errors_store;
 } App;
 
-// ---------- Utilidades ----------
+// ---------- Utilidades ---------- #
 static void console_append(GtkTextView *view, const char *text) {
     GtkTextBuffer *buf = gtk_text_view_get_buffer(view);
     GtkTextIter end;
